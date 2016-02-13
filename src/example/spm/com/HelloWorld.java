f4ffa4beb219518851315397fa2d3873c879c8a9
spmGitHub <joana.iljazi@stud-inf.unibz.it>
2016-02-13 22:10:58 +0200

    added class SimpleCalculator

--- src/example/spm/com/HelloWorld.java
+++ src/example/spm/com/HelloWorld.java
@@ -6,9 +6,18 @@ public class HelloWorld {
 		return "Joana";
 	}
 
+	public static String message(){
+		return "Welcome to my project!";
+	}
+	
 	
 	public static void main(String[] args){
 		String name = getName();
 		System.out.println("Hello World, "+ name);
+		System.out.println(message());
+		int a=2;
+		int b=4;
+		System.out.println(SimpleCalculator.add(a,b));
+		System.out.println(SimpleCalculator.sub(a,b));
 	}
 }
---
+++ src/example/spm/com/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package example.spm.com;
+
+public class SimpleCalculator {
+	
+	public static int add(int a, int b){
+		return a+b;
+	}
+	
+	public static int sub(int a, int b){
+		return a-b;
+	}
+
+}
